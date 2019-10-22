3082a64c07a39d580e8f2e9f4be49d6dfd4999b8
Bruce Zu <bzu@fortinet.com>
2019-10-22 16:27:03 -0700

    Update pf_deploy.sh
    prepare customized directory in host machine for docker container persistent data
    
    And prepare  max_map_count  in VM environment

--- pf_deploy.sh
+++ pf_deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-# Run this bash with pf_common.sh in the same directory somewhere at destination VM.
-# Test with:
-#   sshpass -p 'admin123'  ssh -o "StrictHostKeyChecking no" -o "IdentitiesOnly=yes" admin@167.188.21.37
-#   images is fetche from ${IM_SERVER_USER}@${IM_SERVER}. TODO: fetch them from registeryserver.
-set -eu
+## start/stop FortinetCloudPlatform in FMG VM.
+#
+##  Run this bash with pf_common.sh in the same directory somewhere in destination FMG VM.
+# E.g. /var/deploy/pf
+#
+##  Currently it is using the mocked docker images repostiory to fetch docker images:
+#   sshpass -p 'admin123'  ssh -o "StrictHostKeyChecking no" -o "IdentitiesOnly=yes" admin@10.106.6.53
+#   images is fetched from ${IM_SERVER_USER}@${IM_SERVER}.
+#   TODO: fetch them from offical docker iamges repository once it is ready.
+#
+
+set -eux
 
 function deploy() {
     local I=$(readlink -f $0)
@@ -12,15 +19,22 @@ function deploy() {
 
     local PF_HOME=/var/pf_home
 
-    echo "# set persistant tmp path used by docker compose in BPJ VM"
+    echo "# set persistant tmp path used by docker compose in FMG VM"
     local TMP=/var/tmp/docker_compose_pf
     rm -rf ${TMP}/*
     mkdir -p ${TMP}
     export TMPDIR=${TMP}
 
+    echo "# prepare the persistent storage sub directory"
+    mkdir -p /var/docker/fortifsw/{portal_data,mysql_data,mongodb_data,elasticsearch_data}
+
+    echo "# prepare the max_map_count for elasticsearch"
+    echo 262144 >/proc/sys/vm/max_map_count
+
     echo "# old process: kill"
-    if [[ -f ${PF_HOME}/${YAML} ]]; then
-        docker-compose -f ${PF_HOME}/${YAML} -f ${PF_HOME}/${YAML_VM} down
+
+    if [[ -f ${PF_HOME}/${YAML_VM} ]]; then
+        docker-compose -f ${PF_HOME}/${YAML_VM} down
     fi
     local IDs=$(ps | grep -E "${YAML_VM}" | grep -v "grep" | awk '{print $1}')
     if [ ! -z "$IDs" ]; then
@@ -34,7 +48,7 @@ function deploy() {
     fi
 
     echo "# container: cleaning "
-    containers=$(docker ps -a | grep -E "$A|$B|$C|$D|$E" | awk '{print $1}')
+    containers=$(docker ps -a | grep -E "$A|$B|$C|$E" | awk '{print $1}')
     for c in ${containers}; do
         docker stop $c
         docker rm $c
@@ -42,7 +56,7 @@ function deploy() {
 
     if [[ "$parameter" != "-no" && "$parameter" != "--no-fetch" ]]; then
         echo "# images: cleaning"
-        for i in $(docker images | grep -E "$A|$B|$C|$D|$E" | awk '{print $3}'); do
+        for i in $(docker images | grep -E "$A|$B|$C|$E" | awk '{print $3}'); do
             if [[ "$i" != "IMAGE" ]]; then
                 echo "removing image $i"
                 docker rmi $i
@@ -60,10 +74,10 @@ function deploy() {
         echo "# fetching docker images"
         # create knowhost
         sshpass -p ${IM_SERVER_PASS} ssh -o "StrictHostKeyChecking no" ${IM_SERVER_USER}@${IM_SERVER} ls -lh ${HOME_ON_HOP} &
-        wait
+        wait "$!"
         echo "will fetch the listed image .... need wait with patient."
         sshpass -p ${IM_SERVER_PASS} scp ${IM_SERVER_USER}@${IM_SERVER}:"${HOME_ON_HOP}"/* "${PF_HOME}"/ &
-        wait
+        wait "$!"
         echo -e "# verify integratioin:\n## original md5sum"
         cat "${PF_HOME}/${md5sums}"
         echo "## after fetch, local md5sum:"
@@ -78,7 +92,7 @@ function deploy() {
                 docker tag $IM_ID ${IM[i]}:${TAG[i]}
             fi
         done &
-        wait
+        wait "$!"
     else
         echo "# not fetch images"
     fi
@@ -86,8 +100,11 @@ function deploy() {
     if [[ -f nohup.out ]]; then
         rm nohup.out
     fi
-    nohup docker-compose -f ${PF_HOME}/${YAML} -f ${PF_HOME}/${YAML_VM} up &
-    echo "Success deployed with version $(cat ${PF_HOME}/$version)"
+
+    echo "Deployed with version $(cat ${PF_HOME}/$version). check log in nohup"
+
+    # nohup docker-compose -f ${PF_HOME}/${YAML_VM} up >/dev/null 2>&1 &
+    nohup docker-compose -f ${PF_HOME}/${YAML_VM} up &
 }
 
 function usage() {
@@ -95,7 +112,7 @@ function usage() {
     default: fetch Platform docker images latest version and start Platform
         -h --help: usage.
         -n --no-fetch: not fetch images, still use exiting one.
-        -s --stop: stop running apps of cloud .
+        -s --stop: stop running apps of cloud platform.
     "
 }
 
